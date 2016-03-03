8fc8af359e642dfa73c95c26a783630ff1f198ab
Anjie Scheuren <emailanjie@gmail.com>
2016-03-02 19:09:02 -0600

    functionality to show one show hard coded

--- public/javascripts/currentgeo.js
+++ public/javascripts/currentgeo.js
@@ -1,95 +1,133 @@
-var x = document.getElementById("demo");
-function getLocation() {
-    if (navigator.geolocation) {
-        navigator.geolocation.getCurrentPosition(showPosition, showError);
-    } else {
-        x.innerHTML = "Geolocation is not supported by this browser.";
+$(function() {
+
+  var apiRoot1 = 'https://api.songkick.com/api/3.0/events.json?location=geo:30.2669444,-97.7427778&page=1&per_page=100&min_date=2016-03-04&max_date=2016-03-04&apikey=PTAZie3wbuF6n5dx&jsoncallback=?';
+  var coords = [];
+
+  $.ajax({
+    url: apiRoot1,
+    method: "GET",
+    data: {},
+    dataType: "jsonp"
+  })
+  .done(function(data) {
+    var shows = data.resultsPage.results.event;
+
+    for(var i = 0; i < shows.length; i++) {
+      var artist = shows[i].performance[0].artist.displayName;
+      var venue = shows[i].venue.displayName;
+      var lat = shows[i].venue.lat;
+      var lng = shows[i].venue.lng;
+      var time = moment(shows[i].start.time, "hh:mm a").format("h:mm a");
+      var date = moment(shows[i].start.date, "YYYY-MM-DD").format("dddd, MMMM Do");
+      var datenow = moment().format("dddd, MMMM Do");
+      var timenow = moment().format("hh:mm a");
+
+// function to filter out shows with null values
+      if (venue != "Unknown venue" &&
+        lat != null &&
+        time != "Invalid date" &&
+        time > timenow) {
+        coords.push(lat, lng);
+      }
     }
+    console.log(coords);
+  })
+
+  date_time();
+  getLocation();
+});
+
+function date_time() {
+  now = moment().format('dddd, MMMM Do hh:mm a');
+  document.getElementById('clock').innerHTML = now;
+  setTimeout(function () { date_time(); }, 6000);
+}
+
+function getLocation() {
+  if (navigator.geolocation) {
+      navigator.geolocation.getCurrentPosition(showPosition, showError);
+  } else {
+      x.innerHTML = "Geolocation is not supported by this browser.";
+  }
 }
 
 function successHandler(location) {
-    var message = document.getElementById("message"), html = [];
-    html.push("<img width='256' height='256' src='http://maps.googleapis.com/maps/api/js", location.coords.latitude, ",", location.coords.longitude, "&markers=size:small|color:blue|", location.coords.latitude, ",", location.coords.longitude, "&zoom=14&size=256x256&sensor=false' />");
-    html.push("<p>Longitude: ", location.coords.longitude, "</p>");
-    html.push("<p>Latitude: ", location.coords.latitude, "</p>");
-    html.push("<p>Accuracy: ", location.coords.accuracy, " meters</p>");
-    // message.innerHTML = html.join("");
+  var message = document.getElementById("message"), html = [];
+  html.push("<img width='256' height='256' src='http://maps.googleapis.com/maps/api/js", location.coords.latitude, ",", location.coords.longitude, "&markers=size:small|color:blue|", location.coords.latitude, ",", location.coords.longitude, "&zoom=14&size=256x256&sensor=false' />");
+  html.push("<p>Longitude: ", location.coords.longitude, "</p>");
+  html.push("<p>Latitude: ", location.coords.latitude, "</p>");
+  html.push("<p>Accuracy: ", location.coords.accuracy, " meters</p>");
+  // message.innerHTML = html.join("");
 }
 function errorHandler(error) {
-    alert('Attempt to get location failed: ' + error.message);
+  alert('Attempt to get location failed: ' + error.message);
 }
 navigator.geolocation.getCurrentPosition(successHandler, errorHandler);
 
 function showPosition(position) {
-    lat = position.coords.latitude;
-    lon = position.coords.longitude;
-    latlon = new google.maps.LatLng(lat, lon)
-    mapholder = document.getElementById('mapholder')
-    mapholder.style.height = '350px';
-    mapholder.style.width = '600px';
+  lat = position.coords.latitude;
+  lon = position.coords.longitude;
+  latlon = new google.maps.LatLng(lat, lon)
+  mapholder = document.getElementById('mapholder')
+  mapholder.style.height = '350px';
+  mapholder.style.width = '600px';
 
-    var myOptions = {
-      center:latlon,zoom:14,
-      mapTypeId:google.maps.MapTypeId.ROADMAP,
-      mapTypeControl:false,
-      fullscreenControl: true,
-      navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
-    }
+  var myOptions = {
+    center:latlon,zoom:14,
+    mapTypeId:google.maps.MapTypeId.ROADMAP,
+    mapTypeControl:false,
+    fullscreenControl: true,
+    navigationControlOptions:{style:google.maps.NavigationControlStyle.SMALL}
+  }
 
-    var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
+  var map = new google.maps.Map(document.getElementById("mapholder"), myOptions);
 
-    // listen for the window resize event & trigger Google Maps to update too
-    $(window).resize(function() {
-    // (the 'map' here is the result of the created 'var map = ...' above)
-     google.maps.event.trigger(map, "resize");
-    });
+  // listen for the window resize event & trigger Google Maps to update too
+  $(window).resize(function() {
+  // (the 'map' here is the result of the created 'var map = ...' above)
+   google.maps.event.trigger(map, "resize");
+  });
 
-    var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
+  var marker = new google.maps.Marker({position:latlon,map:map,title:"You are here!"});
 
-    var myCity = new google.maps.Circle({
-      center:latlon,
-      radius:1207.008,
-      strokeColor:"#DD1C1A",
-      strokeOpacity:0.4,
-      strokeWeight:2,
-      fillColor:"#DD1C1A",
-      fillOpacity:0.1
-    });
-      myCity.setMap(map);
-    }
+  var myCity = new google.maps.Circle({
+    center:latlon,
+    radius:1207.008,
+    strokeColor:"#DD1C1A",
+    strokeOpacity:0.4,
+    strokeWeight:2,
+    fillColor:"#DD1C1A",
+    fillOpacity:0.1
+  });
+    myCity.setMap(map);
 
-function showError(error) {
-    switch(error.code) {
-        case error.PERMISSION_DENIED:
-            x.innerHTML = "User denied the request for Geolocation."
-            break;
-        case error.POSITION_UNAVAILABLE:
-            x.innerHTML = "Location information is unavailable."
-            break;
-        case error.TIMEOUT:
-            x.innerHTML = "The request to get user location timed out."
-            break;
-        case error.UNKNOWN_ERROR:
-            x.innerHTML = "An unknown error occurred."
-            break;
-    }
+    function dropMarker() {
+       var latlng = new google.maps.LatLng(30.2694535, -97.7421049);
+       var marker = new google.maps.Marker({
+         position: latlng,
+         icon: 'images/microphone.png',
+         map: map
+       });
+     }
+ dropMarker();
 }
 
-// function to filter out shows with null values
-
-// if
-// venue === unknown venue or
-// date === null or
-// time === invalid date or
-// lat === null or
-// lng === null
-// do not display show
-// else
-// display show
-
-// function to filter shows happening on datenow, after timenow
-
-// function to filter shows within .75 mile of current user's location
+function showError(error) {
+  switch(error.code) {
+    case error.PERMISSION_DENIED:
+      x.innerHTML = "User denied the request for Geolocation."
+      break;
+    case error.POSITION_UNAVAILABLE:
+      x.innerHTML = "Location information is unavailable."
+      break;
+    case error.TIMEOUT:
+      x.innerHTML = "The request to get user location timed out."
+      break;
+    case error.UNKNOWN_ERROR:
+      x.innerHTML = "An unknown error occurred."
+      break;
+  }
+}
 
 // function to display those shows on the map
 
@@ -99,10 +137,10 @@ function showError(error) {
 // var marker = new google.maps.Marker({position:latlon,map:map,icon:'images/headphone.png',title:"You are here!"});
 
 // var infowindow = new google.maps.InfoWindow({
-//         content:"show.date, show.artist, show.venue, show.time"
-//         });
+//   content:"show.date, show.artist, show.venue, show.time"
+//   });
 
-//       google.maps.event.addListener(marker, 'click', function() {
-//         infowindow.open(map,marker);
-//         });
+// google.maps.event.addListener(marker, 'click', function() {
+//   infowindow.open(map,marker);
+//   });
 
--- public/javascripts/main.js
+++ public/javascripts/main.js
@@ -1,55 +1,37 @@
-$(function() {
+// $(function() {
 
-  var apiRoot = 'https://api.songkick.com/api/3.0/events.json?location=geo:30.2669444,-97.7427778&per_page=100&min_date=2016-03-15&max_date=2016-03-20&apikey=PTAZie3wbuF6n5dx&jsoncallback=?';
-  var eventIndex = 0;
+//   var apiRoot1 = 'https://api.songkick.com/api/3.0/events.json?location=geo:30.2669444,-97.7427778&page=1&per_page=100&min_date=2016-03-04&max_date=2016-03-04&apikey=PTAZie3wbuF6n5dx&jsoncallback=?';
 
-  $.ajax({
-      url: apiRoot,
-      method: "GET",
-      data: {},
-      dataType: "jsonp",
-      jsonCallback: "info"
-    })
-  .done(function(data) {
-      var shows = data.resultsPage.results.event;
+//   var venues = [];
 
-      // function formatShowObj(event, i) {
-      //   return {
-      //     artist: shows[i].performance[0].artist.displayName,
-      //     venue: shows[i].venue.displayName,
-      //     latitude: shows[i].venue.lat,
-      //     longitude: shows[i].venue.lng,
-      //     date: shows[i].start.date,
-      //     time: shows[i].start.time
-      //   }
-      // }
+//   $.ajax({
+//     url: apiRoot1,
+//     method: "GET",
+//     data: {},
+//     dataType: "jsonp"
+//   })
+//   .done(function(data) {
+//     var shows = data.resultsPage.results.event;
 
-      // var show = formatShowObj(event, eventIndex);
-      // var time = moment(show.time, "hh:mm a").format("h:mm a");
-      // var date = moment(show.date, "YYYY-MM-DD").format("dddd, MMMM Do");
+//     for(var i = 0; i < shows.length; i++) {
+//       var artist = shows[i].performance[0].artist.displayName;
+//       var venue = shows[i].venue.displayName;
+//       var lat = shows[i].venue.lat;
+//       var lng = shows[i].venue.lng;
+//       var time = moment(shows[i].start.time, "hh:mm a").format("h:mm a");
+//       var date = moment(shows[i].start.date, "YYYY-MM-DD").format("dddd, MMMM Do");
 
-      function date_time() {
-        now = moment().format('dddd, MMMM Do hh:mm a');
-        document.getElementById('clock').innerHTML = now;
-        setTimeout(function () { date_time(); }, 6000);
-      }
-      date_time();
+//       if (venue != "Unknown venue" && lat != null && time != "Invalid date") {
+//         venues.push(lat, lng);
+//       }
+//     }
+//     console.log(venues);
+//   })
+//   function date_time() {
+//     now = moment().format('dddd, MMMM Do hh:mm a');
+//     document.getElementById('clock').innerHTML = now;
+//     setTimeout(function () { date_time(); }, 6000);
+//   }
+//   date_time();
+// });
 
-      for(var i = 0; i < 100; i ++) {
-        var artist = shows[i].performance[0].artist.displayName;
-        var venue = shows[i].venue.displayName;
-        var lat = shows[i].venue.lat;
-        var lng = shows[i].venue.lng;
-        var time = moment(shows[i].start.time, "hh:mm a").format("h:mm a");
-        var date = moment(shows[i].start.date, "YYYY-MM-DD").format("dddd, MMMM Do");
-
-        console.log(artist);
-        console.log(venue);
-        console.log(lat);
-        console.log(lng);
-        console.log(date);
-        console.log(time);
-      }
-    })
-
-})
