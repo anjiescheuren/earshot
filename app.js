e53a68997235fec329da9d46b9227f970ec86b70
Anjie Scheuren <emailanjie@gmail.com>
2016-05-26 14:49:52 -0500

    favicon

--- app.js
+++ app.js
@@ -11,13 +11,13 @@ var routes = require('./routes/index');
 var users = require('./routes/users');
 
 var app = express();
+// uncomment after placing your favicon in /public
+app.use(favicon(__dirname + '/public/favicon.ico'));
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'ejs');
 
-// uncomment after placing your favicon in /public
-app.use(favicon(__dirname + '/public/favicon.ico'));
 app.use(logger('dev'));
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({
